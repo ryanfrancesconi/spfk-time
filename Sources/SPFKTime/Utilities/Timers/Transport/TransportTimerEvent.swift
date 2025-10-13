12277c1bb2aa2a16ce576976f6586afb74c51e1b
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-10-13 15:36:07 -0700

    various updates to shadow tag

--- Sources/SPFKTime/Utilities/Timers/Transport/TransportTimerEvent.swift
+++ Sources/SPFKTime/Utilities/Timers/Transport/TransportTimerEvent.swift
@@ -3,6 +3,7 @@ import Foundation
 public enum TransportTimerEvent {
     case state(TransportTimerPlayState)
     case time(TimeInterval)
+    case complete
 }
 
 public enum TransportTimerPlayState {
