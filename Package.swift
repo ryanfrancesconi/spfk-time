07e80514c49032d6cbdae1a43109ca93248cec23
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-24 17:17:10 -0800

    update Package.swift template to use useLocalDependencies as a switch between local and released (remote)

--- Package.swift
+++ Package.swift
@@ -2,13 +2,21 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
-// Swift target
-private let name: String = "SPFKTime"
-
+private let name: String = "SPFKTime" // Swift target
+private let dependencyNames: [String] = ["SPFKBase", "SPFKUtils", "SPFKTesting"]
+private let dependencyBranch = "main"
+private let useLocalDependencies: Bool = false
 private let platforms: [PackageDescription.SupportedPlatform]? = [
-    .macOS(.v12)
+    .macOS(.v12),
+    .iOS(.v15),
 ]
+
+// MARK: - Reusable Code for single package
+
+private let nameTests: String = "\(name)Tests" // Test target
+private let githubBase = "https://github.com/ryanfrancesconi"
 
 private let products: [PackageDescription.Product] = [
     .library(
@@ -17,38 +25,68 @@ private let products: [PackageDescription.Product] = [
     )
 ]
 
-private let dependencies: [PackageDescription.Package.Dependency] = [
-    .package(name: "SPFKUtils", path: "../SPFKUtils"),
-    .package(name: "SPFKTesting", path: "../SPFKTesting"),
-    
-//     .package(url: "https://github.com/ryanfrancesconi/SPFKUtils", branch: "main"),
-//     .package(url: "https://github.com/ryanfrancesconi/SPFKTesting", branch: "main"),
-    
-    .package(url: "https://github.com/orchetect/TimecodeKit", from: "2.0.11"),
+private var packageDependencies: [PackageDescription.Package.Dependency] {
+     let local: [PackageDescription.Package.Dependency] =
+        dependencyNames.map {
+            .package(name: "\($0)", path: "../\($0)")
+        }
 
-]
+        
+     let remote: [PackageDescription.Package.Dependency] =
+        dependencyNames.map {
+            .package(url: "\(githubBase)/\($0)", branch: dependencyBranch)
+        }
+    
+    return useLocalDependencies ? local : remote
+}
+
+// is there a Sources/[NAME]/Resources folder?
+private var swiftTargetResources: [PackageDescription.Resource]? {
+    // package folder
+    let root = URL(fileURLWithPath: #file).deletingLastPathComponent()
+    
+    let dir = root.appending(component: "Sources")
+        .appending(component: name)
+        .appending(component: "Resources")
+    
+    let exists = FileManager.default.fileExists(atPath: dir.path)
+    
+    return exists ? [.process("Resources")] : nil
+}
+
+private var swiftTargetDependencies: [PackageDescription.Target.Dependency] {
+    let names = dependencyNames.filter { $0 != "SPFKTesting" }
+    
+    return names.map {
+        .byNameItem(name: "\($0)", condition: nil)
+    }
+}
+
+private let swiftTarget: PackageDescription.Target = .target(
+    name: name,
+    dependencies: swiftTargetDependencies,
+    resources: swiftTargetResources
+)
+
+private var testTargetDependencies: [PackageDescription.Target.Dependency] {
+    var array: [PackageDescription.Target.Dependency] = [
+        .byNameItem(name: name, condition: nil),
+    ]
+
+    if dependencyNames.contains("SPFKTesting") {
+        array.append(.byNameItem(name: "SPFKTesting", condition: nil))
+    }
+    
+    return array
+}
+
+private let testTarget: PackageDescription.Target = .testTarget(
+    name: nameTests,
+    dependencies: testTargetDependencies
+)
 
 private let targets: [PackageDescription.Target] = [
-    // Swift
-    .target(
-        name: name,
-        dependencies: [
-            .byNameItem(name: "SPFKUtils", condition: nil),
-            .byNameItem(name: "TimecodeKit", condition: nil)
-        ]
-    ),
-    
-
-    .testTarget(
-        name: "\(name)Tests",
-        dependencies: [
-            .byNameItem(name: name, condition: nil),
-            .byNameItem(name: "SPFKTesting", condition: nil)
-        ],
-        resources: [
-            .process("Resources")
-        ]
-    )
+    swiftTarget, testTarget
 ]
 
 let package = Package(
@@ -56,7 +94,8 @@ let package = Package(
     defaultLocalization: "en",
     platforms: platforms,
     products: products,
-    dependencies: dependencies,
+    dependencies: packageDependencies,
     targets: targets,
     cxxLanguageStandard: .cxx20
 )
+
