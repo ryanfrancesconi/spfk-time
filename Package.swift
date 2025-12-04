57544c2885c88f7d7464eecdf08e73a6945424b0
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-04 07:38:52 -0800

    cleanup

--- Package.swift
+++ Package.swift
@@ -44,36 +44,3 @@ let package = Package(
         ),
     ]
 )
-
-/*
- let name: String = "SPFKTime" // Swift target
- var localDependencies: [RemoteDependency] { [
-     .init(package: .package(url: "\(githubBase)/spfk-base", from: "0.0.1"),
-           product: .product(name: "SPFKBase", package: "spfk-base")),
-     .init(package: .package(url: "\(githubBase)/spfk-utils", from: "0.0.1"),
-           product: .product(name: "SPFKUtils", package: "spfk-utils")),
-     .init(package: .package(url: "\(githubBase)/spfk-testing", from: "0.0.1"),
-           product: .product(name: "SPFKTesting", package: "spfk-testing")),
- ] }
-
- let remoteDependencies: [RemoteDependency] = [
-     .init(package: .package(url: "https://github.com/orchetect/swift-timecode", from: "3.0.0"),
-           product: .product(name: "SwiftTimecode", package: "swift-timecode"))
- ]
- let resources: [PackageDescription.Resource]? = nil
-
- let nameC: String? = nil
- let dependencyNamesC: [String] = []
- let remoteDependenciesC: [RemoteDependency] = []
- var cSettings: [PackageDescription.CSetting]? { [
-     .headerSearchPath("include_private")
- ] }
- var cxxSettings: [PackageDescription.CXXSetting]? { [
-     .headerSearchPath("include_private")
- ] }
- let platforms: [PackageDescription.SupportedPlatform]? = [
-     .macOS(.v12),
-     .iOS(.v15),
- ]
-
- */
